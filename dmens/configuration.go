a11a623e26f43b52a7724393ca77c1357e1515b8
Gkirito <lownkid@outlook.com>
2022-12-27 17:48:11 +0800

    🌟 feat(dmens): add poster

--- dmens/configuration.go
+++ dmens/configuration.go
@@ -1,30 +1,42 @@
 package dmens
 
-type DMensConfiguration struct {
+const (
+	APP_ID_FOR_COMINGCHAT_APP = 0
+	SuiDevNet                 = "https://fullnode.devnet.sui.io"
+	Module                    = "dmens"
+	GasBudGet                 = 1000
+)
+
+type Configuration struct {
 	Name            string
 	FullNodeUrl     string
 	GraphqlUrl      string
 	ContractAddress string
-	// GlobalId        string
+	GlobalProfileId string
 	// ProfileTableId string
 	IsMainNet bool
 }
 
+type PosterConfig struct {
+	Address    string
+	DmensNftId string
+}
+
 var (
-	MainnetConfig = &DMensConfiguration{
+	MainnetConfig = &Configuration{
 		Name:        "mainnet",
 		FullNodeUrl: "https://fullnode.mainnet.sui.io:443",
 		IsMainNet:   true,
 	}
 
-	TestnetConfig = &DMensConfiguration{
+	TestnetConfig = &Configuration{
 		Name:            "testnet",
 		FullNodeUrl:     "https://fullnode.testnet.sui.io:443",
 		GraphqlUrl:      "https://graphql.coming.chat/sui-testnet/graphql",
 		ContractAddress: "0x7a3ff93380660c4fa3ea8df8de13acb2cadf7052",
 	}
 
-	DevnetConfig = &DMensConfiguration{
+	DevnetConfig = &Configuration{
 		Name:            "devnet",
 		FullNodeUrl:     "https://fullnode.devnet.sui.io:443",
 		GraphqlUrl:      "https://graphql.coming.chat/sui-devnet/graphql",
---
+++ dmens/poster.go
@@ -0,0 +1,62 @@
+package dmens
+
+import (
+	"context"
+	"errors"
+	"github.com/coming-chat/go-sui/client"
+	"github.com/coming-chat/go-sui/types"
+	"net/http"
+	"time"
+)
+
+type Poster struct {
+	Configuration
+	chain     *client.Client
+	address   *types.Address
+	packageId *types.HexData
+	PosterConfig
+}
+
+func NewPoster(posterConfig PosterConfig, configuration Configuration) (*Poster, error) {
+	hc := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns:    20,
+			IdleConnTimeout: 20 * time.Second,
+		},
+		Timeout: 15 * time.Second,
+	}
+	ct, _ := client.DialWithClient(configuration.FullNodeUrl, hc)
+	addr, err := types.NewAddressFromHex(posterConfig.Address)
+	if err != nil {
+		return nil, err
+	}
+	packId, err := types.NewHexData(configuration.ContractAddress)
+	if err != nil {
+		return nil, err
+	}
+	return &Poster{
+		Configuration: configuration,
+		chain:         ct,
+		address:       addr,
+		packageId:     packId,
+		PosterConfig:  posterConfig,
+	}, nil
+}
+
+func (p *Poster) IsRegister() bool {
+	if p.DmensNftId == "" {
+		return false
+	}
+	return true
+}
+
+func (p *Poster) ExecTransaction(signedTxn *types.SignedTransaction) (string, error) {
+	response, err := p.chain.ExecuteTransaction(context.Background(), *signedTxn, types.TxnRequestTypeWaitForLocalExecution)
+	if err != nil {
+		return "", err
+	}
+	if response.EffectsCert.ConfirmedLocalExecution {
+		return response.TransactionDigest(), nil
+	}
+	return "", errors.New("exec transaction failed")
+}
