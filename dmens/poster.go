4cffb30c698c7740dd3742880d823f58b27e7137
Gkirito <lownkid@outlook.com>
2022-12-28 11:00:44 +0800

    🔨 refactor(dmens/poster): use wallet-sdk sui chain

--- dmens/poster.go
+++ dmens/poster.go
@@ -1,47 +1,22 @@
 package dmens
 
 import (
-	"context"
-	"errors"
-	"github.com/coming-chat/go-sui/client"
-	"github.com/coming-chat/go-sui/types"
-	"net/http"
-	"time"
+	"github.com/coming-chat/wallet-SDK/core/sui"
 )
 
 type Poster struct {
 	Configuration
-	chain     *client.Client
-	address   *types.Address
-	packageId *types.HexData
 	PosterConfig
+	chain *sui.Chain
 }
 
 func NewPoster(posterConfig PosterConfig, configuration Configuration) (*Poster, error) {
-	hc := &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConns:    20,
-			IdleConnTimeout: 20 * time.Second,
-		},
-		Timeout: 15 * time.Second,
-	}
-	ct, _ := client.DialWithClient(configuration.FullNodeUrl, hc)
-	addr, err := types.NewAddressFromHex(posterConfig.Address)
-	if err != nil {
-		return nil, err
-	}
-	packId, err := types.NewHexData(configuration.ContractAddress)
-	if err != nil {
-		return nil, err
-	}
 	poster := &Poster{
 		Configuration: configuration,
-		chain:         ct,
-		address:       addr,
-		packageId:     packId,
+		chain:         sui.NewChainWithRpcUrl(configuration.FullNodeUrl),
 		PosterConfig:  posterConfig,
 	}
-	err = poster.initialDmensObjecId()
+	err := poster.initialDmensObjecId()
 	if err != nil {
 		return nil, err
 	}
@@ -53,15 +28,4 @@ func (p *Poster) IsRegister() bool {
 		return false
 	}
 	return true
-}
-
-func (p *Poster) ExecTransaction(signedTxn *types.SignedTransaction) (string, error) {
-	response, err := p.chain.ExecuteTransaction(context.Background(), *signedTxn, types.TxnRequestTypeWaitForLocalExecution)
-	if err != nil {
-		return "", err
-	}
-	if response.EffectsCert.ConfirmedLocalExecution {
-		return response.TransactionDigest(), nil
-	}
-	return "", errors.New("exec transaction failed")
 }
