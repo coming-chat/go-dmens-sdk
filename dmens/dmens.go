2b223928b36452b27b5169d2eabd6cc9f06397c5
Gkirito <lownkid@outlook.com>
2022-12-28 12:13:05 +0800

    🌟 feat(dmens/dmens): add post post_with_ref follow unfollow

--- dmens/dmens.go
+++ dmens/dmens.go
@@ -1,5 +1,78 @@
 package dmens
 
-const (
-	dmensModule = "dmens"
+import (
+	"github.com/coming-chat/wallet-SDK/core/base"
+	"github.com/coming-chat/wallet-SDK/core/sui"
 )
+
+const (
+	actionPost = iota
+	ActionRePost
+	ActionQuotePost
+	ActionReply
+	ActionLike
+
+	dmensModule = "dmens"
+
+	functionPost     = "post"
+	functionPostRef  = "post_with_ref"
+	functionFollow   = "follow"
+	functionUnfollow = "unfollow"
+)
+
+func (p *Poster) DmensPost(text string) (*sui.Transaction, error) {
+	return p.chain.BaseMoveCall(
+		p.Address,
+		p.ContractAddress,
+		dmensModule,
+		functionPost,
+		[]any{
+			p.DmensNftId,
+			appIdForComingchatApp,
+			actionPost,
+			text,
+		},
+	)
+}
+
+func (p *Poster) DmensPostWithRef(action int, text, refIdentifier string) (*sui.Transaction, error) {
+	return p.chain.BaseMoveCall(
+		p.Address,
+		p.ContractAddress,
+		dmensModule,
+		functionPostRef,
+		[]any{
+			p.DmensNftId,
+			appIdForComingchatApp,
+			action,
+			text,
+			refIdentifier,
+		},
+	)
+}
+
+func (p *Poster) DmensFollow(addresses base.StringArray) (*sui.Transaction, error) {
+	return p.chain.BaseMoveCall(
+		p.Address,
+		p.ContractAddress,
+		dmensModule,
+		functionFollow,
+		[]any{
+			p.DmensNftId,
+			addresses.Values,
+		},
+	)
+}
+
+func (p *Poster) DmensUnfollow(addresses base.StringArray) (*sui.Transaction, error) {
+	return p.chain.BaseMoveCall(
+		p.Address,
+		p.ContractAddress,
+		dmensModule,
+		functionUnfollow,
+		[]any{
+			p.DmensNftId,
+			addresses.Values,
+		},
+	)
+}
