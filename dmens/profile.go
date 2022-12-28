70d3d0b6b379bc900ed0c9e35044ff3cb09f8ff6
Gkirito <lownkid@outlook.com>
2022-12-28 11:01:07 +0800

    🌟 feat(dmens/profile): finish register func

--- dmens/profile.go
+++ dmens/profile.go
@@ -1,5 +1,10 @@
 package dmens
 
+import (
+	"encoding/json"
+	"github.com/coming-chat/wallet-SDK/core/sui"
+)
+
 const (
 	profileModule    = "profile"
 	FunctionRegister = "register"
@@ -11,10 +16,19 @@ type Profile struct {
 	Avatar string `json:"avatar"`
 }
 
-//func (p *Poster) Register(profile Profile) (*types.TransactionBytes, error) {
-//	profileBytes, err := json.Marshal(profile)
-//	if err != nil {
-//		return nil, err
-//	}
-//	//TODO need use wallet sdk function
-//}
+func (p *Poster) Register(profile Profile) (*sui.Transaction, error) {
+	profileBytes, err := json.Marshal(profile)
+	if err != nil {
+		return nil, err
+	}
+	return p.chain.BaseMoveCall(
+		p.Address,
+		p.ContractAddress,
+		profileModule,
+		FunctionRegister,
+		[]any{
+			p.GlobalProfileId,
+			string(profileBytes),
+			"", //signature is disabled
+		})
+}
