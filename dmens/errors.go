57978a745bb9880c8950132b8e0359b4c81d1fd9
Gkirito <lownkid@outlook.com>
2023-02-02 18:33:12 +0800

    🌟 feat(dmens/profile): new error & checkProfile

--- dmens/errors.go
+++ dmens/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrUserNotRegistered    = errors.New("the user not registered")
 	ErrGetNullConfiguration = errors.New("get remote configuration with null")
+	ErrNotValidPorfile      = errors.New("profile is not valid")
 )
